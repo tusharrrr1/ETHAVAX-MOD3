b1bcfed6a2e5290ae321a883453f9c7ee427be32
tushar <tusharkathuria1897@gmail.com>
2024-06-25 21:33:34 +0530

    updated files

--- contracts/tokens.sol
+++ contracts/tokens.sol
@@ -1,45 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
 
-contract Token {
-    // Public variables
-    string public tokenName = "MyToken";
-    string public abbrv = "MT";
-    uint public totalSupply = 1000;
+pragma solidity ^0.8.20;
 
-    // Mapping variable
-    mapping (address => uint) public balances;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    // Event emitted when tokens are minted
-    event Mint(address indexed account, uint amount);
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
 
-    // Event emitted when tokens are burned
-    event Burn(address indexed account, uint amount);
-
-    // Mint function
-    function mint(address _address, uint _value) public {
-        totalSupply += _value;
-        balances[_address] += _value;
-        emit Mint(_address, _value);
-    }
-
-    // Burn function
-    function burn(address _address, uint _value) public {
-        require(balances[_address] >= _value, "Insufficient balance");
-        totalSupply -= _value;
-        balances[_address] -= _value;
-        emit Burn(_address, _value);
-    }
-
-    // Get balance of an address
-    function getBalance(address _address) public view returns (uint) {
-        return balances[_address];
-    }
-
-    // Transfer tokens from one address to another
-    function transfer(address _from, address _to, uint _value) public {
-        require(balances[_from] >= _value, "Insufficient balance");
-        balances[_from] -= _value;
-        balances[_to] += _value;
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
